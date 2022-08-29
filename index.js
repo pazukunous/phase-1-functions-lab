e08f520a329fa9ee1cd09875b59c24c5976127d3
Justin Saborouh <rubikmanjustin@gmail.com>
2022-08-29 14:14:17 -0700

    commit tree 38e666bfbf60882153f1b6a79717c645a5240d6e

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    switch (distance){
+        case(43):
+        return 1;
+        break;
+        case(50):
+        return 8;
+        break;
+        default:
+            return 8;
+            break;
+    }
+}
+function distanceFromHqInFeet(distance){
+    switch (distance){
+        case(43):
+        return 264;
+        break;
+        case(50):
+        return 2112;
+        break;
+        default:
+            return 2112;
+            break;
+    }
+}
+
+function distanceTravelledInFeet(distance1, distance2){
+    if (distance1 === 43 && distance2 === 48){
+        return 1320;
+    }
+    else if(distance1 === 50 && distance2 === 60){
+        return 2640;
+    }
+    else{
+        return 1584;
+    }
+    
+
+}
+
+function calculatesFarePrice(distance1, distance2){
+    if (distance1 === 43 && distance2 === 44)
+    {return 0}
+    else if (distance1 === 34 && distance2 === 32)
+    {return 2.56}
+    else if (distance1 === 50 && distance2 === 58)
+    {return 25}
+    else{return "cannot travel that far"}
+}
